96eb8f15290f83922c3d825e6b52f1e1b332b506
ahqrt <1217223113@qq.com>
2022-07-30 13:40:16 +0800

    添加clip

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
+use clap::{App, Arg};
 use std::env;
 use std::io::{self, Read, Write};
 const CHUNK_SIZE: usize = 16 * 1024;
 
 fn main() {
-    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
-    let mut total_bytes = 0;
+    let matches = App::new("pipeviewer")
+        .arg(Arg::with_name("infile").help("Read from a file instead of stdin"))
+        .arg(
+            Arg::with_name("outfile")
+                .short('o')
+                .long("outfile")
+                .takes_value(true)
+                .help("Write to a file instead of stdout"),
+        )
+        .arg(Arg::with_name("silent").short('s').long("silent"))
+        .get_matches();
 
+    let infile = matches.value_of("infile").unwrap_or_default();
+    let outfile = matches.value_of("outfile").unwrap_or_default();
+    let silent = if matches.is_present("silent") {
+        true
+    } else {
+        !env::var("PV_SILENT").unwrap_or_default().is_empty()
+    };
+
+    let mut total_bytes = 0;
+    let mut buffer = [0; CHUNK_SIZE];
     loop {
-        let mut buffer = [0; CHUNK_SIZE];
         let num_read = match io::stdin().read(&mut buffer) {
             Ok(0) => break,
             Ok(x) => x,
